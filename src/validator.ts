51a6601ca9a65b088274d0e9a0ff6f05b7515b74
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2024-01-10 19:47:37 +0400

    refactor: validator helpers

--- src/validator.ts
+++ src/validator.ts
@@ -22,35 +22,11 @@ export function validate(node: NodeToProcess, natspec: Natspec): string[] {
         alerts = [...alerts, ...validateParameters(node, natspec)];
     } else if(node instanceof FunctionDefinition) {
         alerts = [...alerts, ...validateParameters(node, natspec)];
-
-        let functionReturns = node.vReturnParameters.vParameters.map(p => p.name);
-        let natspecReturns = natspec.returns.map(p => p.name);
-
-        // Make sure all defined returns have natspec
-        for(let paramName of functionReturns) {
-            if(!natspecReturns.includes(paramName)) {
-                let message = paramName === '' ? '@return missing for unnamed return' : `@return ${paramName} is missing`;
-                alerts.push(message);
-            }
-        }
-    
-        // Make sure there is no natspec defined for non-existing returns
-        for(let paramName of natspecReturns) {
-            if(paramName && !functionReturns.includes(paramName)) {
-                alerts.push(`Missing named return for: @return ${paramName}`);
-            }
-        }
+        alerts = [...alerts, ...validateReturnParameters(node, natspec)];
     } else if(node instanceof ModifierDefinition) {
         alerts = [...alerts, ...validateParameters(node, natspec)];
     } else if(node instanceof StructDefinition) {
-        let members = node.vMembers.map(p => p.name);
-        let natspecMembers = natspec.params.map(p => p.name);
-    
-        for(let memberName of members) {
-            if(!natspecMembers.includes(memberName)) {
-                alerts.push(`@param ${memberName} is missing`);
-            }
-        }
+        alerts = [...alerts, ...validateMembers(node, natspec)];
     } else if(node instanceof VariableDeclaration) {
         // Only the presence of a notice is validated
     }
@@ -71,5 +47,42 @@ function validateParameters(node: ErrorDefinition | FunctionDefinition | Modifie
         }
     }
 
+    return alerts;
+}
+
+function validateReturnParameters(node: FunctionDefinition, natspec: Natspec): string[] {
+    let alerts: string[] = [];
+    let functionReturns = node.vReturnParameters.vParameters.map(p => p.name);
+    let natspecReturns = natspec.returns.map(p => p.name);
+
+    // Make sure all defined returns have natspec
+    for(let paramName of functionReturns) {
+        if(!natspecReturns.includes(paramName)) {
+            let message = paramName === '' ? '@return missing for unnamed return' : `@return ${paramName} is missing`;
+            alerts.push(message);
+        }
+    }
+
+    // Make sure there is no natspec defined for non-existing returns
+    for(let paramName of natspecReturns) {
+        if(paramName && !functionReturns.includes(paramName)) {
+            alerts.push(`Missing named return for: @return ${paramName}`);
+        }
+    }
+
+    return alerts;
+}
+
+function validateMembers(node: StructDefinition, natspec: Natspec): string[] {
+    let alerts: string[] = [];
+    let members = node.vMembers.map(p => p.name);
+    let natspecMembers = natspec.params.map(p => p.name);
+    
+    for(let memberName of members) {
+        if(!natspecMembers.includes(memberName)) {
+            alerts.push(`@param ${memberName} is missing`);
+        }
+    }
+    
     return alerts;
 }
