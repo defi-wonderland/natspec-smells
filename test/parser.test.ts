d8617ce6adf2bdd936977751f19f8b5068fbe779
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2024-01-12 00:11:57 +0400

    test: parser unit tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,171 +1,282 @@
 import { ContractDefinition } from 'solc-typed-ast';
 import { Parser } from '../src/parser';
-import { getFileCompiledSource } from './utils';
+import { getFileCompiledSource, mockNatspec } from './utils';
 
 describe('Parser', () => {
   const parser: Parser = new Parser();
+  let contract: ContractDefinition;
 
-  describe('BasicSample.sol', () => {
-    let contract: ContractDefinition;
-
+  describe('Contract', () => {
     beforeAll(async () => {
-      const compileResult = await getFileCompiledSource('sample-data/BasicSample.sol');
-      contract = compileResult.vContracts[0];
-    });
-
-    it('should parse struct', async () => {
-      const structNode = contract.vStructs.find(({ name }) => name === 'TestStruct')!;
-      const result = parser.parseNodeNatspec(structNode);
-
-      expect(result).toEqual({
-        tags: [
-          {
-            name: 'notice',
-            content: 'Some notice of the struct',
-          },
-        ],
-        params: [],
-        returns: [],
-      });
-    });
-
-    it('should parse constant', async () => {
-      const emptyStringNode = contract.vStateVariables.find(({ name }) => name === '_EMPTY_STRING')!;
-      const result = parser.parseNodeNatspec(emptyStringNode);
-
-      expect(result).toEqual({
-        tags: [
-          {
-            name: 'notice',
-            content: 'Empty string for revert checks',
-          },
-          {
-            name: 'dev',
-            content: `result of doing keccak256(bytes(''))`,
-          },
-        ],
-        params: [],
-        returns: [],
-      });
-    });
-
-    it('should parse a fully natspeced external function', async () => {
-      const functionNode = contract.vFunctions.find(({ name }) => name === 'externalSimple')!;
-      const result = parser.parseNodeNatspec(functionNode);
-
-      expect(result).toEqual({
-        tags: [
-          {
-            name: 'notice',
-            content: 'External function that returns a bool',
-          },
-          {
-            name: 'dev',
-            content: 'A dev comment',
-          },
-        ],
-        params: [
-          {
-            name: '_magicNumber',
-            content: 'A parameter description',
-          },
-          {
-            name: '_name',
-            content: 'Another parameter description',
-          },
-        ],
-        returns: [
-          {
-            name: '_isMagic',
-            content: 'Some return data',
-          },
-        ],
-      });
-    });
-
-    it('should parse a fully natspeced private function', async () => {
-      const functionNode = contract.vFunctions.find(({ name }) => name === 'privateSimple')!;
-      const result = parser.parseNodeNatspec(functionNode);
-
-      expect(result).toEqual({
-        tags: [
-          {
-            name: 'notice',
-            content: 'Private test function',
-          },
-        ],
-        params: [
-          {
-            name: '_magicNumber',
-            content: 'A parameter description',
-          },
-        ],
-        returns: [],
-      });
-    });
-
-    it('should parse multiline descriptions', async () => {
-      const functionNode = contract.vFunctions.find(({ name }) => name === 'multiline')!;
-      const result = parser.parseNodeNatspec(functionNode);
-
-      expect(result).toEqual({
-        tags: [
-          {
-            name: 'notice',
-            content: 'Private test function\n         with multiple\n lines',
-          },
-        ],
-        params: [],
-        returns: [],
-      });
-    });
-
-    it('should parse multiple of the same tag', async () => {
-      const functionNode = contract.vFunctions.find(({ name }) => name === 'multitag')!;
-      const result = parser.parseNodeNatspec(functionNode);
-
-      expect(result).toEqual({
-        tags: [
-          {
-            name: 'notice',
-            content: 'Private test function',
-          },
-          {
-            name: 'notice',
-            content: 'Another notice',
-          },
-        ],
-        params: [],
-        returns: [],
-      });
-    });
-  });
-
-  describe('InterfacedSample.sol', () => {
-    let contract: ContractDefinition;
-
-    beforeAll(async () => {
-      const compileResult = await getFileCompiledSource('sample-data/InterfacedSample.sol');
-      contract = compileResult.vContracts[1];
+      const compileResult = await getFileCompiledSource('sample-data/ParserTest.sol');
+      contract = compileResult.vContracts.find(({ name }) => name === 'ParserTest')!;
     });
 
     it('should parse the inheritdoc tag', async () => {
-      const functionNode = contract.vFunctions.find(({ name }) => name === 'greet')!;
-      const result = parser.parseNodeNatspec(functionNode);
+      const node = contract.vFunctions.find(({ name }) => name === 'viewFunctionNoParams')!;
+      const result = parser.parseNodeNatspec(node);
 
-      expect(result).toEqual({
+      expect(result).toEqual(
+        mockNatspec({
+          inheritdoc: { content: 'IParserTest' },
+          tags: [ {
+            name: 'dev',
+            content: 'Dev comment for the function',
+          }]}
+        ));
+    });
+
+    it('should parse constant', async () => {
+      const node = contract.vStateVariables.find(({ name }) => name === 'SOME_CONSTANT')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(
+        mockNatspec({
         inheritdoc: {
-          content: 'IInterfacedSample',
+          content: 'IParserTest',
+        }
+      }));
+    });
+
+    it('should parse variable', async () => {
+      const node = contract.vStateVariables.find(({ name }) => name === 'someVariable')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        inheritdoc: {
+          content: 'IParserTest',
         },
         tags: [
           {
             name: 'dev',
-            content: 'some dev thingy',
+            content: 'Providing context'
+          }
+        ]})
+      );
+    });
+
+    it('should parse modifier', async () => {
+      const node = contract.vModifiers.find(({ name }) => name === 'someModifier')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'The description of the modifier'
+          }
+        ],
+        params: [
+          {
+            name: '_param1',
+            content: 'The only parameter'
+          }
+        ]
+      }));
+    });
+
+    it('should parse external function', async () => {
+      const node = contract.vFunctions.find(({ name }) => name === 'viewFunctionNoParams')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        inheritdoc: {
+          content: 'IParserTest',
+        },
+        tags: [
+          {
+            name: 'dev',
+            content: 'Dev comment for the function',
+          }
+        ],
+      }));
+    });
+
+    it('should parse private function', async () => {
+      const node = contract.vFunctions.find(({ name }) => name === '_viewPrivate')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'Some private stuff',
+          },
+          {
+            name: 'dev',
+            content: 'Dev comment for the private function',
+          }
+        ],
+        params: [
+          {
+            name: '_paramName',
+            content: 'The parameter name'
+          }
+        ],
+        returns: [
+          {
+            name: '_returned',
+            content: 'The returned value'
+          }
+        ]
+      }));
+    });
+
+    it('should parse multiline descriptions', async () => {
+      const node = contract.vFunctions.find(({ name }) => name === '_viewComplex')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'Some internal stuff\n         Separate line\n         Third one',
+          },
+        ]
+      }));
+    });
+
+    it('should parse multiple of the same tag', async () => {
+      const node = contract.vFunctions.find(({ name }) => name === '_viewDuplicateTag')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'Some internal stuff',
+          },
+          {
+            name: 'notice',
+            content: 'Separate line',
+          },
+        ]
+      }));
+    });
+  });
+
+  describe('Interface', () => {
+    beforeAll(async () => {
+      const compileResult = await getFileCompiledSource('sample-data/ParserTest.sol');
+      contract = compileResult.vContracts.find(({ name }) => name === 'IParserTest')!;
+    });
+
+    it('should parse error', async () => {
+      const node = contract.vErrors.find(({ name }) => name === 'SimpleError')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'Thrown whenever something goes wrong',
           },
         ],
-        params: [],
-        returns: [],
-      });
+      }));
+    });
+
+    it('should parse event', async () => {
+      const node = contract.vEvents.find(({ name }) => name === 'SimpleEvent')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'Emitted whenever something happens',
+          },
+        ],
+      }));
+    });
+
+    it('should parse struct', async () => {
+      const node = contract.vStructs.find(({ name }) => name === 'SimplestStruct')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'A struct holding 2 variables of type uint256',
+          },
+          {
+            content: 'a  The first variable',
+            name: 'member',
+          },
+          {
+            content: 'b  The second variable',
+            name: 'member',
+          },
+          {
+            content: 'This is definitely a struct',
+            name: 'dev',
+          },
+        ],
+      }));
+    });
+
+    // TODO: Parse natspec for enums
+    // it('should parse enum', async () => {
+    //   const node = contract.vEnums.find(({ name }) => name === 'SimpleEnum')!;
+    //   const result = parser.parseNodeNatspec(node);
+
+    //   expect(result).toEqual(mockNatspec({
+    //     tags: [],
+    //   }));
+    // });
+
+    it('should parse external function without parameters', async () => {
+      const node = contract.vFunctions.find(({ name }) => name === 'viewFunctionNoParams')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'View function with no parameters',
+          },
+          {
+            name: 'dev',
+            content: 'Natspec for the return value is missing'
+          }
+        ],
+        returns: [
+          {
+            name: 'The',
+            content: 'returned value'
+          }
+        ]
+      }));
+    });
+
+    it('should parse external function with parameters', async () => {
+      const node = contract.vFunctions.find(({ name }) => name === 'viewFunctionWithParams')!;
+      const result = parser.parseNodeNatspec(node);
+
+      expect(result).toEqual(mockNatspec({
+        tags: [
+          {
+            name: 'notice',
+            content: 'A function with different style of natspec',
+          },
+        ],
+        params: [
+          {
+            name: '_param1',
+            content: 'The first parameter'
+          },
+          {
+            name: '_param2',
+            content: 'The second parameter'
+          }
+        ],
+        returns: [
+          {
+            name: 'The',
+            content: 'returned value'
+          }
+        ]
+      }));
     });
   });
 });
