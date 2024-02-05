41f95cc3399615b7de60a1b4857d90d84568b0cb
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2024-02-05 17:28:50 +0400

    refactor: split validator tests

--- test/validator.test.ts
+++ test/validator.test.ts
@@ -265,14 +265,15 @@ describe('Validator', () => {
     expect(result).toContainEqual(`@param ${paramName2} is missing`);
   });
 
-  it('should ignore receive and fallback', () => {
-    node = contract.vFunctions.find(({ kind }) => kind === 'receive' || kind === 'fallback')!;
-    natspec = {
-      tags: [],
-      params: [],
-      returns: [],
-    };
-    const result = validator.validate(node, natspec);
+  it('should ignore the receive function', () => {
+    node = contract.vFunctions.find(({ kind }) => kind === 'receive')!;
+    const result = validator.validate(node, mockNatspec({}));
+    expect(result).toEqual([]);
+  });
+
+  it('should ignore the callback function', () => {
+    node = contract.vFunctions.find(({ kind }) => kind === 'fallback')!;
+    const result = validator.validate(node, mockNatspec({}));
     expect(result).toEqual([]);
   });
 
